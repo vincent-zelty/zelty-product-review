a46b9efed9513628df96954e91336bc3d8b704c4
Vincent Le Badezet <vincent@zelty.fr>
2024-12-07 16:18:34 +0100

    chore: Init project

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,169 @@
+import { Timeline } from "@/components/ui/timeline";
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+  const data = [
+    {
+      title: "Janvier",
+      content: (
+        <div className="mb-20">
+          <div className="mb-8">
+            <h3 className="text-neutral-800 dark:text-neutral-200 font-bold text-xl">
+              BO
+            </h3>
+            <ul className="text-neutral-800 dark:text-neutral-200 mb-8">
+              <li>🛵 Intégration avec le nouveau Logisticien</li>
+              <li>
+                🍔 Nouvelle page pour afficher les réglements directement depuis
+                le nBO
+              </li>
+            </ul>
+          </div>
+          <div className="mb-8">
+            <h3 className="text-neutral-800 dark:text-neutral-200 font-bold text-xl">
+              API Publique
+            </h3>
+            <ul className="text-neutral-800 dark:text-neutral-200 mb-8">
+              <li>⏱️ Amélioration des temps de réponses</li>
+            </ul>
+          </div>
+          <div className="mb-8">
+            <h3 className="text-neutral-800 dark:text-neutral-200 font-bold text-xl">
+              Caisse
+            </h3>
+            <ul className="text-neutral-800 dark:text-neutral-200 mb-8">
+              <li>
+                💻 Re-organisation des options et appareils (meilleurs
+                lisibilité)
+              </li>
+              <li>📌 Nouvelle page de login et de code pin #newDesign</li>
+              <li>
+                💰 Utilisateurs multi compte peut se connecter sur la caisse et
+                on lui demande sur quel restaurant il veut aller
+              </li>
+            </ul>
             <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+              src="https://assets.aceternity.com/pro/hero-sections.png"
+              alt="hero template"
+              width={500}
+              height={500}
+              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      ),
+    },
+    {
+      title: "Février",
+      content: (
+        <div className="mb-20">
+          <div className="mb-8">
+            <h3 className="text-neutral-800 dark:text-neutral-200 font-bold text-xl">
+              BO
+            </h3>
+            <ul className="text-neutral-800 dark:text-neutral-200 mb-8">
+              <li>🛵 Intégration avec le nouveau Logisticien</li>
+              <li>
+                🍔 Nouvelle page pour afficher les réglements directement depuis
+                le nBO
+              </li>
+            </ul>
+          </div>
+          <div className="mb-8">
+            <h3 className="text-neutral-800 dark:text-neutral-200 font-bold text-xl">
+              API Publique
+            </h3>
+            <ul className="text-neutral-800 dark:text-neutral-200 mb-8">
+              <li>⏱️ Amélioration des temps de réponses</li>
+            </ul>
+            <Image
+              src="https://assets.aceternity.com/features-section.png"
+              alt="hero template"
+              width={500}
+              height={500}
+              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+            />
+          </div>
+          <div className="mb-8">
+            <h3 className="text-neutral-800 dark:text-neutral-200 font-bold text-xl">
+              Caisse
+            </h3>
+            <ul className="text-neutral-800 dark:text-neutral-200 mb-8">
+              <li>
+                💻 Re-organisation des options et appareils (meilleurs
+                lisibilité)
+              </li>
+              <li>📌 Nouvelle page de login et de code pin #newDesign</li>
+              <li>
+                💰 Utilisateurs multi compte peut se connecter sur la caisse et
+                on lui demande sur quel restaurant il veut aller
+              </li>
+            </ul>
+            <Image
+              src="https://assets.aceternity.com/features-section.png"
+              alt="hero template"
+              width={500}
+              height={500}
+              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+            />
+          </div>
+        </div>
+      ),
+    },
+    {
+      title: "Mars",
+      content: (
+        <div className="mb-20">
+          <div className="mb-8">
+            <h3 className="text-neutral-800 dark:text-neutral-200 font-bold text-xl">
+              BO
+            </h3>
+            <ul className="text-neutral-800 dark:text-neutral-200 mb-8">
+              <li>🛵 Intégration avec le nouveau Logisticien</li>
+              <li>
+                🍔 Nouvelle page pour afficher les réglements directement depuis
+                le nBO
+              </li>
+            </ul>
+          </div>
+          <div className="mb-8">
+            <h3 className="text-neutral-800 dark:text-neutral-200 font-bold text-xl">
+              API Publique
+            </h3>
+            <ul className="text-neutral-800 dark:text-neutral-200 mb-8">
+              <li>⏱️ Amélioration des temps de réponses</li>
+            </ul>
+          </div>
+          <div className="mb-8">
+            <h3 className="text-neutral-800 dark:text-neutral-200 font-bold text-xl">
+              Caisse
+            </h3>
+            <ul className="text-neutral-800 dark:text-neutral-200 mb-8">
+              <li>
+                💻 Re-organisation des options et appareils (meilleurs
+                lisibilité)
+              </li>
+              <li>📌 Nouvelle page de login et de code pin #newDesign</li>
+              <li>
+                💰 Utilisateurs multi compte peut se connecter sur la caisse et
+                on lui demande sur quel restaurant il veut aller
+              </li>
+            </ul>
+            <Image
+              src="https://assets.aceternity.com/features-section.png"
+              alt="hero template"
+              width={500}
+              height={500}
+              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+            />
+          </div>
+        </div>
+      ),
+    },
+  ];
+  return (
+    <div className="w-full">
+      <Timeline data={data} />
     </div>
   );
 }
